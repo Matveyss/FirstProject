2d3d02b3f3f949f6b6b0a026dd2968288426dea9
Matvey <PC@PC->
2020-05-29 17:58:27 +0300

    My first project

---
+++ FirstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет Матвей!");
+
+            Console.ReadLine();
+        }
+    }
+}
